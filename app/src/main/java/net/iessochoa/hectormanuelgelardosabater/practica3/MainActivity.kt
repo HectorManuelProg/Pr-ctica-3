98efde3576b9eeacc3a8200e83c369c6040ebf77
Hector <hectormanuel.gs@hotmail.com>
2023-11-04 15:35:44 +0100

    Punto 18

--- app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica3/MainActivity.kt
+++ app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica3/MainActivity.kt
@@ -2,23 +2,52 @@ package net.iessochoa.hectormanuelgelardosabater.practica3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import net.iessochoa.hectormanuelgelardosabater.practica3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val TAG = "mostrar log"
     var num:Int = 0
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+    override fun onPause() {
+        Log.i(TAG, "onPause")
+        super.onPause()
+    }
+    override fun onStop() {
+        Log.i(TAG, "onStop")
+        super.onStop()
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+    override fun onDestroy() {
+        Log.i(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        Log.i(TAG, "onCreate")
             binding.tvNumero.text=num.toString()
 
             binding.btSumaUno.setOnClickListener(){
                 num++
             binding.tvNumero.text=num.toString()
         }
+
     }
 }
 
